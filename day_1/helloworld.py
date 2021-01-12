bf4841660cde25d4435c6834fb4a0c162ae1d1c5
Felipe de Farias <felipedefarias@live.com>
2021-01-12 11:42:21 -0300

    Day 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,31 @@
+# Exemplos
+print(2 + 3)  # addition(+)
+print(3 - 1)  # subtraction(-)
+print(2 * 3)  # multiplication(*)
+print(3 / 2)  # division(/)
+print(3 ** 2) # exponential(**)
+print(3 % 2)  # modulus(%)
+print(3 // 2) # Floor division operator(//)
+
+# verificando tipos
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex number (meio que um concat automatico do js mas nao vira string)
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List (array do js)
+print(type({'name':'Asabeneh'})) # Dictionary (object do js)
+print(type({9.8, 3.14, 2.7}))    # Set (lista nao ordenavel e unica, nao repete valor)
+print(type((9.8, 3.14, 2.7)))    # Tuple (lembre-se que esse tipo é imutável)
+
+# Exercicio 1
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+
+print('Pita')
+print('Tropa do inguiça')
+print('BR')
+
+# naquele pique
